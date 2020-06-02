ebc9a7bc4d27eec48f3de9853d8a287033d9823f
josmer benjamin suero sencion <josmersuero@gmail.com>
2020-06-02 15:32:06 -0400

    added database, owners table, error messages and a couple more stuff.

---
+++ Facturacion.Web/Data/Entities/MaxLenghtAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Facturacion.Web.Data
+{
+    internal class MaxLenghtAttribute : Attribute
+    {
+        private int v;
+        private string errorMessage;
+
+        public MaxLenghtAttribute(int v, string ErrorMessage)
+        {
+            this.v = v;
+            errorMessage = ErrorMessage;
+        }
+    }
+}
---
+++ Facturacion.Web/Data/Entities/Owner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Facturacion.Web.Data
+{
+    public class Owner
+    {
+        public int ID { get; set; }
+
+
+
+        [MaxLength(30, ErrorMessage = "The field {0} only can contain a maximum {1} characters")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+
+        public string Document { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The field {0} only can contain a maximum {1} characters")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The field {0} only can contain a maximum {1} characters")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [MaxLength(12, ErrorMessage = "The field {0} only can contain a maximum {1} characters")]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [MaxLenght(12, ErrorMessage: "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Cellphone")]
+        public string CellPhone { get; set; }
+
+        [MaxLength(100, ErrorMessage = "The field {0} only can contain a maximum {1} characters")]
+        public string Address { get; set; }
+
+        [Display(Name = "Owner")]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Display(Name = "Owner")]
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+    }
+}
